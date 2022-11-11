26b54f266866c6560516a5d435e6ea6db50119f4
hoquenazmul <technazmul.hoque@gmail.com>
2022-11-11 00:53:17 -0500

    deleted program file

--- Program/recursion.py
+++
@@ -1,7 +0,0 @@
-def factorial(num):
-    if num == 1:
-        return 1
-    else:
-        return num * factorial(num-1)
-
-print(factorial(5)) # Output: 120
--- Program/variable.py
+++
@@ -1,7 +0,0 @@
-txt = "Hello World"
-num = 2021
-is_login = True
-string = str(1)
-
-# Assaign Multiple Variable
-name, age = 'John Doe', 30
